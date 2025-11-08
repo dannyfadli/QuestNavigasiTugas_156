4b986f66affaeb39cca0cadc3f9497b2bb1a7fd1
dannyfadli <danyfdly95@gmail.com>
2025-11-08 14:35:04 +0700

    membuar resource dimen padding small

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.navigation.compose)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
