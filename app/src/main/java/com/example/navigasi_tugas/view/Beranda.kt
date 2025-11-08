605b2ceddb7525f49a806629c54c3225e598b4e8
dannyfadli <danyfdly95@gmail.com>
2025-11-08 14:47:09 +0700

    Membuat innerPadding dan Alignment.Center untuk konten

--- app/src/main/java/com/example/navigasi_tugas/view/Beranda.kt
+++ app/src/main/java/com/example/navigasi_tugas/view/Beranda.kt
@@ -1,11 +1,16 @@
 package com.example.navigasi_tugas.view
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 
@@ -27,3 +32,10 @@ fun Beranda(onStartClick: () -> Unit) {
                 )
             )
         }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
